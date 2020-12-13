355c95f36d4bad8045c8e976d7c1a7743795aea1
Guilherme Siquinelli (guiseek) <guiseek@gmail.com>
2020-12-13 10:07:34 -0300

    initial commit

---
+++ apps/sample-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'freecom-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'sample-app';
+}
