cb9e1b43e95c064ceaf1d29c6762f035fe653716
Guilherme Siquinelli (guiseek) <guiseek@gmail.com>
2020-12-18 22:57:24 -0300

    chore(tools): release tools config

---
+++ .cz-config.js
@@ -0,0 +1,91 @@
+module.exports = {
+  types: [
+    { value: 'feat', name: 'feat:     A new feature' },
+    { value: 'fix', name: 'fix:      A bug fix' },
+    { value: 'docs', name: 'docs:     Documentation only changes' },
+    {
+      value: 'refactor',
+      name:
+        'refactor: A code change that neither fixes a bug nor adds a feature',
+    },
+    {
+      value: 'perf',
+      name: 'perf:     A code change that improves performance',
+    },
+    { value: 'test', name: 'test:     Adding missing tests' },
+    {
+      value: 'style',
+      name:
+        'style:    Changes that do not affect the meaning of the code\n            (white-space, formatting, missing semi-colons, etc)',
+    },
+    {
+      value: 'cleanup',
+      name:
+        'cleanup:  A code change that neither fixes a bug nor adds a feature',
+    },
+    {
+      value: 'chore',
+      name: "chore:    Other changes that don't modify src or test files",
+    },
+    { value: 'revert', name: 'revert:   Reverts' },
+    { value: 'wip', name: 'wip:      Work in progress' },
+    { value: 'ci', name: 'ci:       Continuous integration' },
+  ],
+
+  scopes: [
+    { name: 'server', description: 'anything server specific' },
+    { name: 'client', description: 'anything client specific' },
+    { name: 'media', description: 'anything media specific' },
+    { name: 'ui-popover', description: 'anything ui-popover specific' },
+    {
+      name: 'test',
+      description: 'anything test specific (e.g., jest, cypress)',
+    },
+    {
+      name: 'repo',
+      description: 'anything related to managing the repo itself',
+    },
+    { name: 'misc', description: 'misc stuff' },
+  ],
+
+  allowTicketNumber: false,
+  isTicketNumberRequired: false,
+  ticketNumberPrefix: 'TICKET-',
+  ticketNumberRegExp: '\\d{1,5}',
+
+  // it needs to match the value for field type. Eg.: 'fix'
+  scopeOverrides: {
+    fix: [
+      { name: 'merge' },
+      { name: 'style' },
+      { name: 'e2eTest' },
+      { name: 'unitTest' },
+    ],
+  },
+  // override the messages, defaults are as follows
+  messages: {
+    type: "Select the type of change that you're committing:",
+    scope: '\nDenote the SCOPE of this change (optional):',
+    // used if allowCustomScopes is true
+    customScope: 'Denote the SCOPE of this change:',
+    subject:
+      'Write a SHORT, IMPERATIVE (lowercase) description of the change:\n',
+    body:
+      'Provide a LONGER description of the change (optional). Use "|" to break new line:\n',
+    breaking: 'List any BREAKING CHANGES (optional):\n',
+    footer:
+      'List any ISSUES CLOSED by this change (optional). E.g.: #31, #34:\n',
+    confirmCommit: 'Are you sure you want to proceed with the commit above?',
+  },
+
+  allowCustomScopes: false,
+  allowBreakingChanges: ['feat', 'fix'],
+  // skip any questions you want
+  skipQuestions: ['ticketNumber'],
+
+  // limit subject length
+  subjectLimit: 100,
+  // breaklineChar: '|', // It is supported for fields body and footer.
+  // footerPrefix : 'ISSUES CLOSED:'
+  // askForBreakingChangeFirst : true, // default is false
+};
